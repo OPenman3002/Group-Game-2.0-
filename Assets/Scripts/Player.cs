fd7b447066e22fe4f7a7f5922952b7d8e1004d9a
OWEN PENMAN <1736953@student.fife.ac.uk>
2018-04-24 09:55:04 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float MovementSpeed;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+           
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        HandleMovement(horizontal,vertical);
+
+		
+	}
+    private void HandleMovement(float horizontal, float vertical)
+    {
+        myRigidBody.velocity = new Vector2(horizontal*MovementSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, vertical * MovementSpeed);
+
+        
+    }
+
+}
